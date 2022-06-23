5f63c4938f4f4bdd6b743205a2a916b1557655cd
Mustapha Ibrahim <Stexe@msn.com>
2022-06-23 23:52:21 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b1fd08ff`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=b1fd08ff`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
